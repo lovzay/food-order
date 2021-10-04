93aa29b4afea77b5ad2130fd5ceee61657219fbc
lovzay <60774660+lovzay@users.noreply.github.com>
2021-10-04 11:24:33 +0800

    part 1

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 All Rights Reserved, Restaurant, Developed by - <a href="#"> Group 3</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+        
+    </body>
+</html>
